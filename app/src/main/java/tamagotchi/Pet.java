3253e43a30589bdfcb6b2b5d4f4c5e80b2a99577
Nekkson <durnernils@gmail.com>
2022-11-21 09:53:55 +0100

    rewrote play() method to fit OOP, removed getters and setters, modified fields

--- app/src/main/java/tamagotchi/Pet.java
+++ app/src/main/java/tamagotchi/Pet.java
@@ -1,108 +1,44 @@
 package tamagotchi;
 
+import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public abstract class Pet {
+public abstract class Pet implements Serializable {
     protected String name;
-    // protected String sex_;
-    protected String stage = "Baby";
-    protected int hunger = 3; // the value is initialized with 3 so that the pet is a little hungry when created
+    // protected String sex;
+    protected LocalDateTime birthday;
     protected Food favoriteFood;
-    protected int hygiene = 10;
-    protected int attention = 10;
-    protected int health = hygiene + attention - hunger;
-    protected LocalDateTime birthday, lastLogout_;
-    protected Time age_;
+    protected Toy favoriteToy;
+    protected int dirtiness;
+    protected int boredom;
+    protected int hunger;
 
-    public Pet(){
-    }
-
-    public Pet(String name, String sex){
+    public Pet(String name, Food favoriteFood, Toy favoriteToy){
         this.name = name;
         this.birthday = LocalDateTime.now();
-    }
-    private LocalDateTime stringToLocalDateTime(String str){
-        LocalDateTime dateTime = LocalDateTime.parse(str);
-        return dateTime;
+        this.favoriteFood = favoriteFood;
+        this.favoriteToy = favoriteToy;
     }
 
-    public Pet(String name, String sex, int hunger, int hygiene, int attention, String birthday, String lastLogout){
-        this.name = name;
-        // this.sex = sex;
-        this.hunger = hunger;
-        this.hygiene = hygiene;
-        this.attention = attention;
-        this.birthday = stringToLocalDateTime(birthday);
-        lastLogout_ = stringToLocalDateTime(lastLogout);
-        checkRange();
+    protected Time computeAge(){
+        return new Time(
+                Duration.between(birthday, LocalDateTime.now()).toDays(),
+                Duration.between(birthday, LocalDateTime.now()).toHours(),
+                Duration.between(birthday, LocalDateTime.now()).toMinutes());
     }
-
-    public abstract String getType();
-
-    abstract String randomFavFood();
-    abstract String randomFavToy();
-
-    abstract String getFavoriteFood();
-
-    abstract String getFavoriteToy();
-
-
-    public String get_name(){ return name; }
-
-    public int get_hunger(){
-        return hunger;
-    }
-
-    public int get_hygiene(){
-        return hygiene;
-    }
-
-    public int get_attention(){
-        return attention;
-    }
-
-    LocalDateTime getBirthDay(){ return birthday; }
-
-    LocalDateTime getLastLogout(){ return lastLogout_; }
-    Time getAge(){return age_;}
-
-    void setAge(){
-        age_ = new Time(Duration.between(birthday, LocalDateTime.now()).toDays(), Duration.between(birthday, LocalDateTime.now()).toHours(), Duration.between(birthday, LocalDateTime.now()).toMinutes());
-        if (age_.days_ >= 2 && age_.days_ < 4){
-            stage = "Child";
+    protected String computeStage(){
+        Time age = computeAge();
+        if (age.days_ < 3){
+            return "Child";
         }
-        else if (age_.days_ >= 4 && age_.days_ < 6){
-            stage = "Teenager";
+        else if (age.days_ < 6){
+            return "Teenager";
         }
-        else if (age_.days_ >= 6 && age_.days_ < 12){
-            stage = "Adult";
+        else if (age.days_ < 12){
+            return "Adult";
         }
-        else if (age_.days_ >= 12){
-            stage = "Senior";
-        }
-    }
-    private void checkRange(){
-        if (hunger < 0){
-            hunger = 0;
-        }
-        if (hunger > 10){
-            hunger = 10;
-        }
-        if (hygiene < 0){
-            hygiene = 0;
-        }
-        if (hygiene > 10){
-            hygiene = 10;
-        }
-        if (attention < 0){
-            attention = 0;
-        }
-        if (attention > 10){
-            attention = 10;
-        }
-        health = hunger + hygiene + attention;
-
+        return "Senior";
     }
 
     public void feed(Food food){
@@ -111,55 +47,89 @@ public abstract class Pet {
             System.out.println("I am not hungry :P");}
         else {
             if (favoriteFood == food) {
-                hunger += 5;
-                attention += 1;
+                boredom += 1;
                 checkRange();
             } else {
-                hunger += 3;
                 checkRange();
             }
-            hygiene -= 2;
+            dirtiness -= 2;
             checkRange();
         }
     }
 
     public void clean(String cleaning){
-        if(hygiene == 10){
+        if(dirtiness == 10){
             System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             System.out.println(name +" is clean enough.");
         }
         else {
-            hygiene += 3;
+            dirtiness += 3;
             checkRange();
         }
 
     }
 
-    public void play(String favorite){
-
-        if (attention == 10){
+    public void play(Toy toy) {
+// when would the pet not want to play? consider adding attribute boredom
+        if (boredom == 0) {
             System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-            System.out.println(name +" doesn't want to play with you anymore.");}
-        else {
-            if (getFavoriteToy().equals(favorite)) {
-                attention += 5;
+            System.out.println("I don't want to play with you anymore.");
+            if (toy == favoriteToy) {
+                boredom += 5;
                 checkRange();
             } else {
-                attention += 3;
+                boredom += 3;
                 checkRange();
             }
-            hunger -= 2;
-            hygiene -= 2;
+            dirtiness -= 2;
             checkRange();
         }
     }
 
+    private void checkRange(){
+        if (hunger < 0){
+            hunger = 0;
+        }
+        if (hunger > 10){
+            hunger = 10;
+        }
+        if (dirtiness < 0){
+            dirtiness = 0;
+        }
+        if (dirtiness > 10){
+            dirtiness = 10;
+        }
+        if (boredom < 0){
+            boredom = 0;
+        }
+        if (boredom > 10){
+            boredom = 10;
+        }
+    }
     void printInfo(){
         System.out.println("*********************************************");
-        setAge();
-        System.out.println("Name: " + name + "\t Age: " + age_.days_ + "D " + age_.hours_ + "H "+ age_.minutes_ + "M" + "\t Stage: " + stage);
-        System.out.println("Hunger: " + hunger + "\t Hygiene: " + hygiene + "\t Attention: " + attention + "\t Health: "+ health);
+        System.out.println("Name: " + name + "\t Age: " + computeAge().days_ + "D " + computeAge().hours_ + "H "+ computeAge().minutes_ + "M" + "\t Stage: " + computeStage());
+        System.out.println("Hunger: " + hunger + "\t Hygiene: " + dirtiness + "\t Attention: " + boredom);
         System.out.println("*********************************************");
     }
 
+    @Override
+    public String toString() {
+        return "Name:" + name + "\nBirthday: " + birthday +
+                "\nFavorite food: " + favoriteFood + "\nFavorite toy: " + favoriteToy +
+                "\nDirtiness: " + dirtiness + "\nBoredom: " + boredom + "\nHunger: " + hunger;
+    }
+
+}
+
+class Dog extends Pet {
+    Dog(String name) {
+        super(name, new Steak(), new Ball());
+    }
+}
+
+class Cat extends Pet{
+    Cat(String name){
+        super(name, new Tuna(), new Yarn());
+    }
 }
